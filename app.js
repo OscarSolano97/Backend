29de36bf863d2af994cc60b9c475afa87748645c
Oscar Solano <mao090997@gmail.com>
2026-06-30 13:57:27 -0500

    feat: se agrega el puerto universal

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 const app = express();
 const port = 3000;
+//const host = '10.5.225.150';
 
 app.get('/', (req, res) => {
   res.json({ message: 'Bienvenido al sistema de gestión.' });
@@ -14,6 +15,6 @@ app.get('/programas', (req, res) => {
   res.json({ programas: ['Ética', 'Matemáticas', 'Programación', 'Comunicación', 'Inglés'] });
 });
 
-app.listen(port, () => {
+app.listen(port,"0.0.0.0", () => {
   console.log(`Servidor escuchando en el puerto ${port}`);
 });
